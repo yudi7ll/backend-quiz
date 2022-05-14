0a3eff0978fb1a74478cadd15ffd8f4d9a840fb6
Yudi <madewahyudi22@gmail.com>
2022-05-14 10:40:10 +0800

    setup docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:16-alpine 
+
+WORKDIR /app
+COPY . /app
+RUN npm i
+# RUN npm run build
+
+EXPOSE 3000
+
+CMD "npm" "run" "start:dev"
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 import server from './server';
+import mysql from 'mysql';
 
+const connection = mysql.createConnection({
+    host: 'mysql',
+    user: 'root',
+    password: 'secret',
+    database: 'quiz'
+});
+
+connection.connect();
 
 // Constants
 const serverStartMsg = 'Express server started on port: ',
-        port = (process.env.PORT || 3000);
+    port = (process.env.PORT || 3000);
 
 // Start server
 server.listen(port, () => {
