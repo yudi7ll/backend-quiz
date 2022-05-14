1c4c24185f3dd943d67a774823a7a639275641d6
Yudi <madewahyudi22@gmail.com>
2022-05-14 11:14:20 +0800

    fix eslint

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
-import './pre-start'; // Must be the first import
-import logger from 'jet-logger';
-import server from './server';
-import mysql from 'mysql';
+import './pre-start' // Must be the first import
+import logger from 'jet-logger'
+import server from './server'
+import { Sequelize } from 'sequelize'
+// import mysql from 'mysql'
 
-const connection = mysql.createConnection({
-    host: 'mysql',
-    user: 'root',
-    password: 'secret',
-    database: 'quiz'
-});
+// const connection = mysql.createConnection({
+//   host: 'mysql',
+//   user: 'root',
+//   password: 'secret',
+//   database: 'quiz'
+// })
 
-connection.connect();
+// connection.connect()
+
+const sequelize = new Sequelize('quiz', 'root', 'secret', {
+  host: 'mysql',
+  dialect: 'mysql'
+})
+
+console.log(sequelize.authenticate())
 
 // Constants
-const serverStartMsg = 'Express server started on port: ',
-    port = (process.env.PORT || 3000);
+const serverStartMsg = 'Express server started on port: '
+const port = (process.env.PORT || 3000)
 
 // Start server
 server.listen(port, () => {
-    logger.info(serverStartMsg + port);
-});
+  logger.info(serverStartMsg + port)
+})
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,10 @@
-import { Router } from 'express';
-import userRouter from './user-router';
-
+import { Router } from 'express'
 
 // Export the base-router
-const baseRouter = Router();
+const baseRouter = Router()
 
 // Setup routers
-baseRouter.use('/users', userRouter);
+baseRouter.use('/test', () => console.log('TEST'))
 
 // Export default.
-export default baseRouter;
+export default baseRouter
